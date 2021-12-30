df1961f69a0dce7d4947dad52e3b3385c37d8701
ACJ <gitlab@acjs.net>
2021-12-30 22:45:56 +0100

    Add current (old) files from production server

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ My homepage, I guess.
 
 Previously, various (sub)domains had their own private repositories:
 
-| Domain                               | Repository                                                                                  |
-|--------------------------------------|---------------------------------------------------------------------------------------------|
-| alexanderchristiaanjacob.com         | `git@gitlab.com:acj/alexanderchristiaanjacob.com.git`                                       |
-| help.alexanderchristiaanjacob.com    | `git@gitlab.com:acj/help.alexanderchristiaanjacob.com.git`                                  |
-| kill.alexanderchristiaanjacob.com    | `git@gitlab.com:acj/kill.alexanderchristiaanjacob.com.git`                                  |
-| support.alexanderchristiaanjacob.com | `git@gitlab.com:acj/support.alexanderchristiaanjacob.com.git`                               |
-| contact.alexanderchristiaanjacob.com | I have no idea where the repository for `contact.alexanderchristiaanjacob.com` is (lolwut?) |
-| a22b.com                             | I have no idea where the repository for `a22b.com` is (lolwut?)                             |
+| Domain                               | Repository                                                    |
+|--------------------------------------|---------------------------------------------------------------|
+| alexanderchristiaanjacob.com         | `git@gitlab.com:acj/alexanderchristiaanjacob.com.git`         |
+| help.alexanderchristiaanjacob.com    | `git@gitlab.com:acj/help.alexanderchristiaanjacob.com.git`    |
+| follow.alexanderchristiaanjacob.com  | unknown                                                       |
+| kill.alexanderchristiaanjacob.com    | `git@gitlab.com:acj/kill.alexanderchristiaanjacob.com.git`    |
+| support.alexanderchristiaanjacob.com | `git@gitlab.com:acj/support.alexanderchristiaanjacob.com.git` |
+| contact.alexanderchristiaanjacob.com | unknown                                                       |
+| a22b.com                             | unknown                                                       |
---
+++ www/follow.alexanderchristiaanjacob.com/index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html dir="ltr" lang="en">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+<meta charset="utf-8">
+<meta property="fb:admins" content="509248955">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="description" property="og:description" content="Follow Alexander Christiaan Jacob (on Twitter and/or Google+).">
+<meta property="og:image" content="http://follow.alexanderchristiaanjacob.com/apple-touch-icon-precomposed.png">
+<meta name="keywords" content="follow, Twitter, Google+, social, sharing, Yin &amp; Yang">
+<meta name="msapplication-TileColor" content="#000000">
+<meta property="og:title" content="Follow Me">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://follow.alexanderchristiaanjacob.com/">
+<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+<link rel="author" href="http://alexanderchristiaanjacob.com/" title="Alexander Christiaan Jacob">
+<link rel="canonical" href="http://follow.alexanderchristiaanjacob.com/">
+<link rel="fluid-icon" href="http://follow.alexanderchristiaanjacob.com/icon-512x512.png" title="Follow ACJ">
+<link rel="shortcut icon" href="favicon.ico">
+<title>Follow Alexander Christiaan Jacob</title>
+<style>
+*{border:0;font-size:1em;margin:0;outline:0;padding:0;text-decoration:none}
+
+a{color:#fff}
+body,html{display:block;height:100%}
+footer,main{display:block}
+footer{bottom:0;margin:0 62px 0 0;padding:1em;position:absolute}
+h1{font-size:1.5em;font-style:italic;font-weight:normal;line-height:1;padding:.75em}
+html{background:#000;color:#999;font:100%/1.5 'Times New Roman',Times,serif,sans-serif;overflow:hidden}
+
+h1 a:before{content:'↩ '}
+
+:root{text-rendering:optimizeLegibility}
+
+.logo{background:url('icon-512x512.png') #000 no-repeat;height:512px;left:50%;margin:-256px 0 0 -256px;position:absolute;top:50%;width:512px}
+.logo a{display:block;height:512px;overflow:hidden;position:absolute;text-indent:300px;top:0;width:256px}
+.logo .twitter{left:256px}
+
+@keyframes rotating{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}
+@-moz-keyframes rotating{from{-moz-transform:rotate(0deg)}to{-moz-transform:rotate(360deg)}}
+@-o-keyframes rotating{from{-o-transform:rotate(0deg)}to{-o-transform:rotate(360deg)}}
+@-webkit-keyframes rotating{from{-webkit-transform:rotate(0deg)}to{-webkit-transform:rotate(360deg)}}
+
+.logo{animation:rotating 12s linear infinite}
+.logo{-moz-animation:rotating 12s linear infinite}
+.logo{-o-animation:rotating 12s linear infinite}
+.logo{-webkit-animation:rotating 12s linear infinite}
+
+</style>
+<script src="//use.typekit.net/mcl4hff.js"></script>
+<script>
+
+try{Typekit.load();}catch(e){}
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36264471-3']);
+  _gaq.push(['_setDomainName', 'alexanderchristiaanjacob.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</head>
+<body>
+<header>
+<h1><a href="//alexanderchristiaanjacob.com/">Alexander Christiaan Jacob</a></h1>
+</header>
+<main class="logo"><a class="googleplus" href="https://plus.google.com/111837838556461503437">Google+</a><a class="twitter" href="https://twitter.com/ACJ">Twitter</a></main>
+<footer>The <cite>Twitter</cite> logo and <cite>Google+</cite> logo are registered trademarks of their respective owners. Obviously, this is not the way you’re supposed to display them. I do not try to steal or mock anything; it’s merely an artistic expression. There’s also a Yin &amp; Yang symbol in there, but I don’t think Buddhism cares about intellectual copyright much.</footer>
+</body>
+</html>
