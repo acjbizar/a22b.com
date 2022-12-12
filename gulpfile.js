1caea45372233d3fff2ebba3c3282fbb6a7675ea
ACJ <gitlab@acjs.net>
2022-12-12 19:43:20 +0100

    Make templating work

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+
+"use strict";
+
+const gulp = require('gulp');
+const twig = require('gulp-twig');
+const data = require('gulp-data');
+const fs = require('fs');
+
+// Compile Twig templates to HTML
+gulp.task('templates', function() {
+    return gulp.src(['./templates/**/*.twig', '!templates/_*.twig', '!templates/base.html.twig'], { dot: true })
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('./data/set.json'));
+        }))
+        .pipe(twig({
+            errorLogToConsole: true,
+            extname: false,
+            filters: [
+                {
+                    name: "u",
+                    func: function (args) {
+                        return args.codePointAt(0).toString(16).padStart(4, '0');
+                    }
+                }
+            ]
+        }))
+        .pipe(gulp.dest('./www')); // output the rendered files to the "dist" directory
+});
